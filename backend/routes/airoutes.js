4c035073954aeb7668650fd14124d9038548c70e
Mridul012 <iammridul012@gmail.com>
2026-04-11 15:11:00 +0530

    feat: add AI invoice parsing, reminder emails, and dashboard insights

--- backend/routes/airoutes.js
+++ backend/routes/airoutes.js
@@ -1,14 +1,4 @@
-// const express= require("express");
-// const {parseInvoiceFromText,generateRemainderEmail,getDashboardSummary} = require("../controllers/aicontroller");
-// const {protect} = require("../middlewares/authmiddleware");
-// const router = require("./authroutes");
 
-
-// router.post("/parse-text",protect,parseInvoiceFromText);
-// router.post("/generate-remainder",protect,generateRemainderEmail);
-// router.post("/dashboard-summary",protect,getDashboardSummary);
-
-// module.exports = router;
 
 const express = require("express");
 
